b2591ff218dc5478c99cbb9fe28819e4791fff1f
mox3r <mox3r.box@gmail.com>
2023-06-08 17:10:25 +0300

    added fret example

---
+++ Examples_c#/examples_003_fret/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.Write("Введите числитель: ");
+// int num1 = Console.ReadLine();
+// Console.Write("Введите знаменатель: ");
+// int num2 = Console.ReadLine();
+
+double num1 = 33;
+double num2 = 11;
+if (num2 == 0)
+{
+    Console.WriteLine("Ошибка001: на ноль делить нельзя!");
+}
+else
+{
+    Console.Write("Частное равно: ");
+    Console.WriteLine(num1/num2);
+}
