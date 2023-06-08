075d37565d034ca4ca2dc8dc0ed76d81482c90fe
mox3r <mox3r.box@gmail.com>
2023-06-08 16:08:14 +0300

    add 001 Hello Console Project

---
+++ Examples_c#/examples_001_helloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Представья, смертный! ");
+Console.WriteLine ("Тебя зовут: ");
+string username = Console.ReadLine();
+if (username == "mox3r")
+{
+    Console.WriteLine("Приветствую тебя, повелитель!");
+}
+else
+{
+    Console.WriteLine("Будь моим гостем, ");
+    Console.WriteLine(username);
+}
