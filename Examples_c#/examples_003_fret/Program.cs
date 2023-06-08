a8b6f973314701add3f15e2e5c643c7b63dc1c87
mox3r <mox3r.box@gmail.com>
2023-06-08 17:22:32 +0300

    added Random modify

--- Examples_c#/examples_003_fret/Program.cs
+++ Examples_c#/examples_003_fret/Program.cs
@@ -3,8 +3,12 @@
 // Console.Write("Введите знаменатель: ");
 // int num2 = Console.ReadLine();
 
-double num1 = 33;
-double num2 = 11;
+double num1 = new Random().Next(-100,1001); // Рандом -100 - 1000
+Console.Write("Делимое равно: ");
+Console.WriteLine(num1);
+double num2 = new Random().Next(-100, 101);
+Console.Write("Делитель равен: ");
+Console.WriteLine(num2);
 if (num2 == 0)
 {
     Console.WriteLine("Ошибка001: на ноль делить нельзя!");
