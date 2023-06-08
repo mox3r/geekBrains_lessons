e24a0d7bbdc98d4fb5679650baba92eba0a909f1
mox3r <mox3r.box@gmail.com>
2023-06-08 17:39:20 +0300

    added base.cs

---
+++ Examples_c#/examples_004_base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 4;
+int c = 17;
+int d = 3;
+int max =a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write ("max = ");
+Console.WriteLine(max);
