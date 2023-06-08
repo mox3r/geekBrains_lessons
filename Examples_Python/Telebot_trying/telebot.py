a91d7a193792d3c15923f0ee8780436a42aa1079
mox3r <mox3r.box@gmail.com>
2023-06-08 21:22:15 +0300

    Added telebot with reminder about programming

---
+++ Examples_Python/Telebot_trying/telebot.py
@@ -0,0 +1,28 @@
+import time
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = "insert_token_here"
+MSG = "{}, занимайся, блеать!"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.fullname
+    logging.info(f'{user_id=} {user_full_name=}, {time.asctime()}')
+    await message.replay(f"Привет, {user_full_name}")
+
+    for i in range(7):
+        time.sleep(60*60*24)
+
+        await bot.send_message(user_id, MSG.format(user_name))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
