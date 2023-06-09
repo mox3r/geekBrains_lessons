cb04bd7de1a6421157436f0b4a9500e7ef93b474
mox3r <mox3r.box@gmail.com>
2023-06-09 16:23:08 +0300

    add 003, modify code 002

--- Examples_c#/Seminar/002/Program.cs
+++ Examples_c#/Seminar/002/Program.cs
@@ -1,11 +1,11 @@
 ﻿//Сравнивает является ли первое число квадратом второго
 System.Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите второе число: ");
+System.Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Проверка 'первое ли квадрат второго?': ");
-int temp = num2 * num2;
-if (num1 == temp)
+
+if (num1 == num2*num2)
 {
     System.Console.WriteLine("Да");
 }
---
+++ Examples_c#/Seminar/003/Program.cs
@@ -0,0 +1,35 @@
+﻿//Число в день недели
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (num == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+if (num == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+if (num == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+if (num == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+if (num == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+if (num == 7)
+{
+    System.Console.WriteLine("Воскресение");
+}
+if (num < 1 || num > 7)
+{
+    System.Console.WriteLine("Дней недели всего 7 :)");
+}
