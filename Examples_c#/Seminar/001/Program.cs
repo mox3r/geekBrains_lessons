eb8af3f6b25a12010ce94b545f79aee975f5ca12
mox3r <mox3r.box@gmail.com>
2023-06-09 15:38:29 +0300

    Iniitial commit, quiz 001

---
+++ Examples_c#/Seminar/001/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Программа возведения целого числа в квадрат */
+Console.Write("Введите целое число: ");
+string num1 = Console.ReadLine();
+int num = int.Parse(num1);
+Console.Write("Квадрат этого числа равен: ");
+Console.WriteLine (num*num);
