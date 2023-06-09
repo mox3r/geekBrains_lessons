85530bf1d92c6959d68359e48bc5e1278067ee85
mox3r <mox3r.box@gmail.com>
2023-06-09 19:11:46 +0300

    add HW tasks

---
+++ Examples_c#/Homework/Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+switch (a % 2)
+{
+    case 1:
+        System.Console.WriteLine("Нечетное");
+        break;
+    case 0:
+        System.Console.WriteLine("Четное");
+        break;
+}
