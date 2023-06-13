8721d215759bd4a305e595f5fc910235eeb206ab
mox3r <mox3r.box@gmail.com>
2023-06-13 06:19:12 +0300

    Converter exercise

---
+++ Examples_c#/Exercises_simpleCode/Value_Converter/Program.cs
@@ -0,0 +1,6 @@
+﻿double RURToUSD = 82.64;
+double BYNToUSD = 2.9495;
+Console.Write("Введите колличество USD: ");    
+double USD = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"{USD} USD = " + USD * RURToUSD + " RUR");
+System.Console.WriteLine($"{USD} USD = " + USD * BYNToUSD + " BYN");
