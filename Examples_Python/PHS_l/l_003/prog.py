294d779fb0f640294196c29bac11c59fc1fc999f
mox3r <mox3r.box@gmail.com>
2023-06-14 15:51:51 +0300

    PHS_lessons

---
+++ Examples_Python/PHS_l/l_003/prog.py
@@ -0,0 +1,21 @@
+# Операторы условий
+# x = 0
+# if x == 0:
+#     print('if')
+# elif x > 0:
+#     print('elif')
+# else:
+#     print('else')
+
+# x = 'a'
+
+# if x == 0:
+#     print('Ошибка! На ноль делить нельзя!')
+#     x = 1
+# elif type(x) == type(5) or type(x) == type(5.5):
+#     print('Допустимое значение!')
+# else:
+#     print("Недопустимый тип данных!")
+#     x = 1
+
+# print(100 / x)
