d18c47f6457b57a8af6c187dedda7d4259a4e639
mox3r <mox3r.box@gmail.com>
2023-06-17 12:47:53 +0300

    s3_HW

---
+++ Examples_c#/Homework/HW_Seminar_003/Task009/Program.cs
@@ -0,0 +1,21 @@
+﻿double InputMsg(string message)
+{
+    System.Console.Write(message);
+    return double.Parse(Console.ReadLine()!);
+}
+double DistVector(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double pow2 = Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2);
+    System.Console.WriteLine(Math.Round(Math.Sqrt(pow2), 2));
+    return 0;
+}
+
+double xa = InputMsg("Точка А. Введите х: ");
+double ya = InputMsg("Точка А. Введите y: ");
+double za = InputMsg("Точка А. Введите z: ");
+
+double xb = InputMsg("Точка B. Введите х: ");
+double yb = InputMsg("Точка B. Введите y: ");
+double zb = InputMsg("Точка B. Введите z: ");
+
+DistVector(xa, ya, za, xb, yb, zb);
---
+++ Examples_c#/Homework/HW_Seminar_003/Task010/Program.cs
@@ -0,0 +1,21 @@
+﻿int InpMSG(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int PowRow(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(Math.Pow(i, 3));
+        if (i < num)
+            System.Console.Write(", ");
+    }
+    return 0;
+}
+
+System.Console.WriteLine("Кубический степенной ряд.");
+int num = InpMSG("Введите количество чисел: ");
+
+PowRow(num);
