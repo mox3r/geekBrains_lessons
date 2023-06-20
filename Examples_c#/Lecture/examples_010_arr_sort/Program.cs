2c2aae8032431b1cb5841e319650376bf6d837ec
mox3r <mox3r.box@gmail.com>
2023-06-20 22:26:16 +0300

    Lc 009-010

---
+++ Examples_c#/Lecture/examples_010_arr_sort/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 13, 52, 34, 7, 3, 89, 3, 24, 9, 4, 6, 7, 8, 5, 5 };
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1) System.Console.Write(", ");
+        else System.Console.Write(" ");
+    }
+}
+
+void arrSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPos]) minPos = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minPos];
+        arr[minPos] = temp;
+    }
+}
+
+PrintArray(arr);
+System.Console.WriteLine("\n");
+arrSort(arr);
+PrintArray(arr);
