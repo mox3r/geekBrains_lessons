f2cdca73d7637d1efc287692453ca887be7868b9
mox3r <mox3r.box@gmail.com>
2023-06-21 20:32:43 +0300

    015-016

---
+++ Examples_c#/Seminar/Seminar_004/015/Program.cs
@@ -0,0 +1,18 @@
+﻿int InputNum(string msg)
+{
+    System.Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+int SummCounter(int summ)
+{
+    int result = summ;
+    for (int i = summ - 1; i >= 0; --i)
+    {
+        result += i;
+    }
+    return result;
+}
+System.Console.WriteLine("Сумма всех чисел числа.");
+int summ = InputNum("Введите число: ");
+int result = SummCounter(summ);
+System.Console.WriteLine($"Результат: {result}");
