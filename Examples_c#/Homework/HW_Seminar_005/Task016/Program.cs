a3c57c1f7ee99d716d560fc4c3ec118c7ae59336
mox3r <mox3r.box@gmail.com>
2023-06-25 20:15:01 +0300

    bugfix

--- Examples_c#/Homework/HW_Seminar_005/Task016/Program.cs
+++ Examples_c#/Homework/HW_Seminar_005/Task016/Program.cs
@@ -1,8 +1,8 @@
-﻿System.Console.WriteLine("Массив вещественных чисел. Найти разницу между макс и мин значениями.");
+﻿Console.WriteLine("Массив вещественных чисел. Найти разницу между макс и мин значениями.");
 
 int InputSize(string msg)
 {
-    System.Console.Write(msg);
+    Console.Write(msg);
     return int.Parse(Console.ReadLine()!);
 }
 
@@ -25,27 +25,28 @@ void FillArray(double[] arr)
 
 void PrintArray(double[] arr)
 {
-    System.Console.Write("[");
+    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         Console.Write(arr[i]);
-        if (i < arr.Length - 1) System.Console.Write(", ");
-        else System.Console.WriteLine("]");
+        if (i < arr.Length - 1) Console.Write(", ");
+        else Console.WriteLine("]");
     }
 }
 
 void PosMaxMinDef(double[] arr)
 {
-    int i = 0;
     int posMax = 0;
     int posMin = 0;
-    for ( ; i < arr.Length; i++)
+
+    for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i] > arr[posMax]) posMax = i;
         else if (arr[i] < arr[posMin]) posMin = i;
     }
-    System.Console.WriteLine($"Максимум = {arr[posMax]}, минимум = {arr[posMin]};");
-    System.Console.WriteLine($"Разница между максимальным и минимальным значениями равна: {arr[posMax] - arr[posMin]}");
+
+    Console.WriteLine($"Максимум = {arr[posMax]}, минимум = {arr[posMin]};");
+    Console.WriteLine($"Разница между максимальным и минимальным значениями равна: {arr[posMax] - arr[posMin]}");
 }
 
 int size = InputSize("Введите размер массива: ");
