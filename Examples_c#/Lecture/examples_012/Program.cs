76666ab3a0e7b6e606616ba3f2a7360f654b9c6d
mox3r <mox3r.box@gmail.com>
2023-06-27 19:17:45 +0300

    pict lecture 012

--- Examples_c#/Lecture/examples_012/Program.cs
+++ Examples_c#/Lecture/examples_012/Program.cs
@@ -24,3 +24,34 @@
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 };
+
+void PrintArray(int[,] pic)
+{
+    for (int rows = 0; rows < pic.GetLength(0); rows++)
+    {
+        for (int cols = 0; cols < pic.GetLength(1); cols++)
+        {
+            if (pic[rows, cols] == 0) System.Console.Write($" ");
+            //System.Console.Write(pic[rows,cols]);
+            else System.Console.Write($"#");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+
+    }
+}
+
+PrintArray(pic);
+FillImage(13,13);
+PrintArray(pic);
