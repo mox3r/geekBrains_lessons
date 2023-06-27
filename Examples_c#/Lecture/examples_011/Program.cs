61610276cfd70dbfb5e48fc0988cd80a0af03193
mox3r <mox3r.box@gmail.com>
2023-06-27 18:50:43 +0300

    lect

---
+++ Examples_c#/Lecture/examples_011/Program.cs
@@ -0,0 +1,18 @@
+﻿int[,] table = new int[3,5];
+
+for (int rows = 0; rows < table.GetLength(0); rows++)
+{
+    for (int cols = 0; cols < table.GetLength(1); cols++)
+    {
+        table[rows,cols] = new Random().Next(10);
+    }
+}
+
+for (int rows = 0; rows < table.GetLength(0); rows++)
+{
+    for (int cols = 0; cols < table.GetLength(1); cols++)
+    {
+        Console.Write($"{table[rows,cols]} ");
+    }
+    System.Console.WriteLine();
+}
