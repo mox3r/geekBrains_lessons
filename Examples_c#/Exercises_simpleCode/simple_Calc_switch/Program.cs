c77010fa22240d3c375420d17c4aaa83525f8a89
mox3r <mox3r.box@gmail.com>
2023-06-27 21:28:34 +0300

    simple calc

---
+++ Examples_c#/Exercises_simpleCode/simple_Calc_switch/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Простейший калькулятор.");
+
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int num1 = InputNum("Введите число а: ");
+int num2 = InputNum("Введите число b: ");
+
+Console.WriteLine(
+    "Выберите действие: \n" +
+    "1. a + b \n" +
+    "2. a - b \n" +
+    "3. a * b \n" +
+    "4. a / b \n"
+    );
+
+int sw = InputNum("Ваш выбор: ");
+
+switch (sw)
+{
+    case (1):
+        Console.WriteLine($"a + b = {num1 + num2}");
+        break;
+    case (2):
+        Console.WriteLine($"a - b = {num1 - num2}");
+        break;
+    case (3):
+        Console.WriteLine($"a * b = {num1 * num2}");
+        break;
+    case (4):
+        if (num2 == 0) Console.WriteLine("Ну куда ты на ноль делишь то??");
+        else Console.WriteLine($"a / b = {num1 / num2}");
+        break;
+    default:
+        Console.WriteLine("Я хз че ты хочешь!");
+        break;
+}
