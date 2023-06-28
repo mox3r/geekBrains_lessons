d859766126875c0d5e25836800edbe4512e9b202
mox3r <mox3r.box@gmail.com>
2023-06-28 19:17:24 +0300

    bugfix

--- Examples_c#/Seminar/Seminar_006/027/Program.cs
+++ Examples_c#/Seminar/Seminar_006/027/Program.cs
@@ -9,16 +9,14 @@ int InputNum(string msg)
 void Fibon(int num)
 {
     int temp = 0, temp1 = 1, temp2 = 0;
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i <= num; i++)
     {
         System.Console.Write(temp + " ");
         temp2 = temp + temp1;
         temp = temp1;
         temp1 = temp2;
     }
-    System.Console.WriteLine();
-    System.Console.WriteLine($"Сумма числового ряда равна {temp2}");
-}
+ }
 
 int num = InputNum("Введите число: ");
 Fibon(num);
