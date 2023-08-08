7205e3d4a154623edf6c4ee0a26ecef3b437250c
mox3r <mox3r.box@gmail.com>
2023-08-08 11:51:35 +0300

    Restructure

--- Examples_Python/spec/p2/prog.py
+++
@@ -1,6 +0,0 @@
-n = 120
-resAll = n // 6
-
-res = (resAll, resAll* 4, resAll)
-tuple_res = tuple(res)
-print(tuple_res)
--- Examples_c#/Seminar/Seminar_009/037/Program.cs
+++ Examples_c#/Seminar/Seminar_009/037/Program.cs
@@ -1,58 +1,18 @@
-﻿int InputNumber(string message)
-{
-    Console.Write(message);
-    return int.Parse(Console.ReadLine()!);
-}
-
-int[,] Create2DArray(int sizeM, int sizeN)
-{
-    return new int[sizeM, sizeN];
-}
-
-void Fill2DArray(int[,] array, int minValue, int maxValue)
-{
-    Random rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = rnd.Next(minValue, maxValue + 1);
-}
-
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+﻿using System.Text;
+string Vowel2Index(string str)
+  {
+    for(int i = 0; i < str.Length; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j]}\t");
-        Console.WriteLine();
+      if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+        str[i] == 'o' || str[i] == 'u')
+        {
+            StringBuilder sb = new StringBuilder(str);
+            sb[i] = i+1;
+            str = sb.ToString();
+        }
+        
     }
-}
+    return str;
+  }
 
-
-
-while (true)
-{
-    Console.Clear();
-    Console.WriteLine("");
-    int sizeM, sizeN, minValue, maxValue;
-    try
-    {
-        sizeM = InputNumber("Введите число строк: ");
-        sizeN = InputNumber("Введите число столбцов: ");
-        minValue = InputNumber("Введите минимальное значение в массиве: ");
-        maxValue = InputNumber("Введите максимальное значение в массиве: ");
-    }
-    catch (Exception)
-    {
-        Console.WriteLine("Вы ввели неверное значение!");
-        Console.ReadLine();
-        continue;
-    }
-
-    int[,] array = Create2DArray(sizeM, sizeN);
-    Fill2DArray(array, minValue, maxValue);
-    Print2DArray(array);
-    Console.WriteLine("Результирующий массив:");
-
-
-    Console.ReadLine();
-}
+  Console.Write(Vowel2Index("this is my string"));
